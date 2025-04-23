64c1d206360751000da49da9ef2f1931e43b6600
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-04-22 23:15:19 -0400

    update checklistmodal.css

--- components/Home/CheckInListModal.jsx
+++ components/Home/CheckInListModal.jsx
@@ -32,51 +32,53 @@ export default function CheckInListModal({
             />
           </button>
           <h2>Your Check-Ins</h2>
-          {checkIns.length === 0 ? (
-            <p className="no-checkins-message">It looks a little empty...</p>
-          ) : (
-            <ul className="check-ins-list">
-              {checkIns.map((checkIn) => (
-                <li key={checkIn.id} className="check-in-list-item">
-                  <div className="check-in-list-item-header">
-                    {checkIn.emojiUrl && (
-                      <img
-                        className="check-in-list-item-feeling"
-                        src={checkIn.emojiUrl}
-                        alt={`${checkIn.text} emoji`}
-                      />
-                    )}
-                    <div className="check-in-list-item-date">
-                      {checkIn.timestamp &&
-                        checkIn.timestamp.toDate().toLocaleString()}
-                    </div>
-                  </div>
-                  <div className="check-in-list-item-body">
-                    {isEditingCheckIn?.id === checkIn.id ? (
-                      <EditingCheckInsListItem
-                        updateCheckIn={updateCheckIn}
-                        isEditingCheckIn={isEditingCheckIn}
-                        setIsEditingCheckIn={setIsEditingCheckIn}
-                        editCheckInText={editCheckInText}
-                        setEditCheckInText={setEditCheckInText}
-                      />
-                    ) : (
-                      <>
-                        <p className="check-in-list-item-snippet">
-                          {checkIn.text}
-                        </p>
-                        <DefaultCheckInsListItem
-                          checkIn={checkIn}
-                          deleteCheckIn={deleteCheckIn}
-                          editCheckIn={editCheckIn}
+          <div className="checkin-list-scroll-wrapper">
+            {checkIns.length === 0 ? (
+              <p className="no-checkins-message">It looks a little empty...</p>
+            ) : (
+              <ul className="check-ins-list">
+                {checkIns.map((checkIn) => (
+                  <li key={checkIn.id} className="check-in-list-item">
+                    <div className="check-in-list-item-header">
+                      {checkIn.emojiUrl && (
+                        <img
+                          className="check-in-list-item-feeling"
+                          src={checkIn.emojiUrl}
+                          alt={`${checkIn.text} emoji`}
                         />
-                      </>
-                    )}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
+                      )}
+                      <div className="check-in-list-item-date">
+                        {checkIn.timestamp &&
+                          checkIn.timestamp.toDate().toLocaleString()}
+                      </div>
+                    </div>
+                    <div className="check-in-list-item-body">
+                      {isEditingCheckIn?.id === checkIn.id ? (
+                        <EditingCheckInsListItem
+                          updateCheckIn={updateCheckIn}
+                          isEditingCheckIn={isEditingCheckIn}
+                          setIsEditingCheckIn={setIsEditingCheckIn}
+                          editCheckInText={editCheckInText}
+                          setEditCheckInText={setEditCheckInText}
+                        />
+                      ) : (
+                        <>
+                          <p className="check-in-list-item-snippet">
+                            {checkIn.text}
+                          </p>
+                          <DefaultCheckInsListItem
+                            checkIn={checkIn}
+                            deleteCheckIn={deleteCheckIn}
+                            editCheckIn={editCheckIn}
+                          />
+                        </>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </>
